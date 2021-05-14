3913ed75aa3b3a2a7cddfa52ce63156c0cddd6d3
Harsh Panchal <hpanchal092@gmail.com>
2021-05-14 14:46:32 -0700

    Feature: Added cog load and unload commands

---
+++ cogs/owner.py
@@ -0,0 +1,41 @@
+from discord.ext import commands
+
+class OwnerCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx, *, cog: str):
+        try:
+            self.bot.load_extension(f"cogs.{cog}")
+        except Exception as e:
+            await ctx.send(f"Error loading {cog}\n{type(e).__name__} - {e}")
+        else:
+            await ctx.send(f"Successfully loaded {cog}")
+
+    @commands.command()
+    @commands.is_owner()
+    async def unload(self, ctx, *, cog: str):
+        try:
+            self.bot.unload_extension(f"cogs.{cog}")
+        except Exception as e:
+            await ctx.send(f"Error unloading {cog}\n{type(e).__name__} - {e}")
+        else:
+            await ctx.send(f"Successfully unloaded {cog}")
+
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx, *, cog: str):
+        try:
+            self.bot.unload_extension(f"cogs.{cog}")
+            self.bot.load_extension(f"cogs.{cog}")
+        except Exception as e:
+            await ctx.send(f"Error reloading {cog}\n{type(e).__name__} - {e}")
+        else:
+            await ctx.send(f"Successfully reloaded {cog}")
+
+
+def setup(bot):
+    bot.add_cog(OwnerCog(bot))
